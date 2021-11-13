3e7eb602834a01c0c9f12042790131a06def89c1
ElianeRiveraCastillo22 <elianercast@gmail.com>
2021-11-13 16:13:16 -0500

    avance con el css

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { example } from './data.js';
 import data from './data/lol/lol.js';/*libera nuestra data*/
 //import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
-console.log(example, data);
+console.log(example)
 /*  para todo lo que tenga que ver con mostrar los datos en la pantalla(con la interaccion del DOM) */ 
