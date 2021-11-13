f9455e57ba99b81a464e0022800fee15a4ec9ff8
ElianeRiveraCastillo22 <elianercast@gmail.com>
2021-11-12 21:48:16 -0500

    hice los botones

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/lol/lol.js';/*libera nuestra data*/
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
-
 console.log(example, data);
+/*  para todo lo que tenga que ver con mostrar los datos en la pantalla(con la interaccion del DOM) */ 
