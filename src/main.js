827496851804cd97f0d223027fd54d15e2551ea8
ElianeRiveraCastillo22 <elianercast@gmail.com>
2021-11-15 01:49:52 -0500

    intentamos jalar data- Jessi

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import data from './data/ghibli/ghibli.js';/*libera nuestra data*/
 //import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 console.log(example)
+//console.log(data.films[0].people[0])
 /*  para todo lo que tenga que ver con mostrar los datos en la pantalla(con la interaccion del DOM) */ 
+
+
+
+
+
+
