26669246cb9da20309137f12b79015314131bfca
ElianeRiveraCastillo22 <elianercast@gmail.com>
2021-11-22 20:17:54 -0500

    logramos filtrado con Deysi

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-
+import{infoDirector} from './data.js';
 //console.log(example)
 import data from './data/ghibli/ghibli.js';/*libera nuestra data*/
 //console.log(data.films[0].people[0])
@@ -7,8 +7,12 @@ import data from './data/ghibli/ghibli.js';/*libera nuestra data*/
 const btnTitle = document.querySelector("#btn-title");
 const btnFilmografia = document.querySelector("#btn-filmografia");
 const btnDuracion = document.querySelector("#btn-duracion");
-const btnDirector = document.querySelector("#btn-director");
+
 const btnPersonajes = document.querySelector("#btn-personajes");
+const btnContainer= document.querySelector(".botones")
+const btnDirector = document.querySelector("#btn-director");
+const btnProductor= document.querySelector("#btn-productor");
+const btnEstreno= document.querySelector("btn-estreno")
 
 //const {films} = data;
 //console.log(films);
@@ -18,6 +22,7 @@ const cards=document.querySelector(".apareceInf");
 
 window.addEventListener('DOMContentLoaded',function(){
 displayMenuItems(infoData)
+//displayFilmsButtons();
 //movieOptions(infoData)
 });
 
@@ -29,10 +34,61 @@ function displayMenuItems(menuItems){
    <figure class="clase">
    <img src="${pelicula.poster}" alt="${pelicula.title}">
    </figure></article>`;
-}) 
+}) ;
 cards.innerHTML= template;
 //console.log(template)
 } 
+
+ //despliegue de pelicula{}
+
+btnTitle.addEventListener('change',function(){
+    mostrarPelicula(infoData);
+
+});
+
+function mostrarPelicula(menuItems){
+    let template="";
+    menuItems.forEach(pelicula=>{
+        //console.log(pelicula)
+        if(pelicula.title===btnTitle.value)
+        template = `<article class="template-card">
+        <figure class="clase">
+        <img src="${pelicula.poster}" alt="${pelicula.title}"
+        </figure>
+        <div>
+        <h2>${pelicula.title}</h2>
+        <h2>${pelicula.director}</h2>
+        <h2>${pelicula.producer}</h2>
+        <h2>${pelicula.release_date}</h2>
+        <h2>${pelicula.rt_score}</h2>
+        <h2>${pelicula.description}</h2>
+        </div>`;
+    })
+    cards.innerHTML=template
+    //console.log(template)
+}
+    //seleccionar pelicula
+let titulos =[];
+
+infoData.forEach(pelicula=>{
+    titulos.push(pelicula.title);
+    /* let option=ducument.createElement('option');
+    option.value=pelicula.title;
+    option.innerText=pelicula.title;
+    btnTitle.appendChild(option); 
+})
+title.sort
+
+    
+
+
+
+
+
+
+
+
+ 
 
 
  /* function movieOptions(movieObj){ 
