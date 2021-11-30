83ec252a558e7960757e8cc4978d8f8908500448
Jessi Polar <jessipolar1977@gmail.com>
2021-11-30 18:25:11 -0500

    pasaron los test

--- src/data.js
+++ src/data.js
@@ -10,6 +10,13 @@ export const anotherExample = () => {
 
 // se crea las funciones para que el usuario pueda iteractuar (independientemente del DOM)
 
+const quitarDuplicados = (arrayData) => {
+  let uniqueArray = arrayData.filter(function(item, pos) {
+    return arrayData.indexOf(item) == pos;
+  })
+  uniqueArray.sort();
+  return uniqueArray;
+}
 
 
 export const ordenarEstreno = (arrayData) => {
@@ -20,8 +27,71 @@ export const ordenarEstreno = (arrayData) => {
      
   })
   //Array.from(new Set(estreno));
-  const uniqueSetEstreno = new Set(estreno);
+  /*const uniqueSetEstreno = new Set(estreno);
   const uniqueEstreno = [...uniqueSetEstreno];
   uniqueEstreno.sort(function(a,b){return b-a});
-  return uniqueEstreno;
+  return uniqueEstreno;*/
+  return quitarDuplicados(estreno).reverse();
 };
+
+// Score de pelicula
+
+export const ordenarScore = (arrayData) => {
+  let scoreDePelicula = [];
+
+  arrayData.forEach(pelicula => {
+      scoreDePelicula.push(pelicula.rt_score);
+   
+  })
+  //Array.from(new Set(productores));
+  /*const uniqueSetScore = new Set(scoreDePelicula);
+  const uniqueScore = [...uniqueSetScore];
+  uniqueScore.sort(function(a,b){return a-b});*/
+  return quitarDuplicados(scoreDePelicula).reverse();
+};
+
+// Productores
+export const ordenarProductores = (arrayData) => {
+  let productores = [];
+
+  arrayData.forEach(pelicula => {
+      productores.push(pelicula.producer);
+    
+  })
+  //Array.from(new Set(productores));
+  /*const uniqueSetProductores = new Set(productores);
+  const uniqueProductores = [...uniqueSetProductores];
+  uniqueProductores.sort();*/
+  return quitarDuplicados(productores);
+
+}
+
+// Ordenar directores
+export const ordenarDirectores = (arrayData) => {
+  let directores = [];
+
+  arrayData.forEach(pelicula => {
+      directores.push(pelicula.director);
+      
+  })
+
+  //Array.from(new Set(directores));
+  /*const uniqueSetDirectores = new Set(directores);
+  const uniqueDirectores = [...uniqueSetDirectores];
+  uniqueDirectores.sort();*/
+  return quitarDuplicados(directores);
+
+}
+
+// Seleccionar pelicula, imagenes, vehiculos y locaciones
+
+export const ordenarTitulos = (arrayData) => {
+  let titulos = [];
+
+  arrayData.forEach(pelicula => {
+      titulos.push(pelicula.title);
+          
+  })
+  return titulos.sort();
+
+}
